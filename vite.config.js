168cc769c9e51e7bd8a8130e66ea419fe1064281
Danish Veer Rana <113288694+Danishveerrana@users.noreply.github.com>
2025-04-13 15:53:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/About-me-dannie.github.io/',
+  base: '/about-me/',
   plugins: [react()],
 });
